4143cc8473ea970fb175e97eca555aaf7aaf5184
leetanat03 <leetanat03@gmail.com>
2025-01-22 23:57:38 +0700

    basic

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 
 // data
-import db from './_db.js';
+import db from './_db.js'
 
 // types
-import { typeDefs } from './schema.js';
+import { typeDefs } from './schema.js'
 
 // resolvers
 const resolvers = {
@@ -13,13 +13,40 @@ const resolvers = {
     games() {
       return db.games
     },
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id)
+    },
     authors() {
       return db.authors
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id)
     },
     reviews() {
       return db.reviews
     },
-  }
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id)
+    }
+  },
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.game_id === parent.id)
+    }
+  },
+  Review: {
+    author(parent) {
+      return db.authors.find((a) => a.id === parent.author_id)
+    },
+    game(parent) {
+      return db.games.find((g) => g.id === parent.game_id)
+    }
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.author_id === parent.id)
+    }
+  },
 }
 
 // server setup
